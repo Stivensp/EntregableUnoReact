fe9c17e2b02b754cb38c6a9fbd6655f49bb8988a
Stiven <delleore1@gmail.com>
2023-11-21 18:56:21 -0500

    modified:   src/components/QuoteBox.jsx

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -3,8 +3,8 @@ const QuoteBox = ({ quote, handleChangeQuote }) => {
     console.log(quote)
   return (
     <article className="quotebox">
-        <h1 className="quotebox___tittle">GALLETAS DE LA FORTUNA</h1>
-        <button className="button" onClick={handleChangeQuote}>Probar mi suerte</button>
+        <h1 className="quotebox___tittle">FORTUNE COOKIES</h1>
+        <button className="button" onClick={handleChangeQuote}>Try my luck</button>
         <img className="img" src="\images\Rectangle 2.png" alt="" />
         <div className="container"><p className="ps">{quote.phrase}</p>   </div>
         <div className="autor">{quote.author}</div>
